38a6ebd99c7f037368431832fc90aea78a227d16
yann <yannlefranc330@gmail.com>
2023-04-10 12:05:33 +0200

    Petit programme the witcher

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    static void thewitcher(String ciri) {
+        System.out.println(ciri + "");
+    }
+    /*
+    ciri est un parametre laorsque la méthode est appeller, cela transmet un prénom    */
+    public static void main(String[] args) {
+        thewitcher("yennefer");
+        thewitcher("ciri");
+        thewitcher("Baron");
+    }
+}
